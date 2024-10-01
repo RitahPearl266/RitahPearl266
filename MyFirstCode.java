553d5166f58f8b7e02adad795d2419de57a8019d
RitahPearl266 <ritahb266@gmail.com>
2024-10-01 10:29:07 +0300

    MyFirstCode.java

--- MyFirstCode.java
+++ MyFirstCode.java
@@ -1,6 +1,6 @@
 public class MyFirstCode {
     public static void main( String [] args){
-        System.out.println("am biira ritah");
+        System.out.println("Am Ritah Pearl, I like Coding Java Programs!.");
     }
 
     
